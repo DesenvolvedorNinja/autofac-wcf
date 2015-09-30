aac37709f38ae63d5d82aec9a4aa3007a8a200a2
Romulo <rcianci@gmail.com>
2015-09-30 17:13:43 -0300

    Código-fonte do exempo de como utilizar o Autofac em conjunto com WCF publicado no site http://desenvolvedor.ninja

---
+++ Global.asax.cs
@@ -0,0 +1,57 @@
+﻿using Autofac;
+using Autofac.Integration.Wcf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace AutofacDemoWCF
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var builder = new ContainerBuilder();
+
+            // Register your service implementations.
+            builder.RegisterType<AutofacDemoWCF.Service1>();
+            builder.RegisterType<Soma>().As<ICalculo>().InstancePerLifetimeScope();
+
+            // Set the dependency resolver.
+            AutofacHostFactory.Container = builder.Build();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
